344cd35dc9e9b95034a81028ebc05375a26c1b0a
Mahmoud Khudairi <mahmoudkhudairi11@gmail.com>
2023-06-13 20:34:33 +0300

    Fix Module Importing

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const ShahidDRM = require("./routes/ShahidDRM");
+const ShahidDRM = require("./routes/ShahidDRM.js");
 const app = express();
 
 app.use("/widevine-drm/shahid", ShahidDRM);
