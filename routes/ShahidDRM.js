4b1ac09786868484fc08f04cb0e4e8e5ad259015
Mahmoud Khudairi <mahmoudkhudairi11@gmail.com>
2023-06-13 20:33:33 +0300

    Use 'https' module Instead of Fetch API

--- routes/ShahidDRM.js
+++ routes/ShahidDRM.js
@@ -1,14 +1,22 @@
+const https = require("https");
+
 module.exports = (_, res) => {
 
-  fetch(
+  https.get(
     "https://api2.shahid.net/proxy/v2.1/playout/new/drm?request=%7B%22assetId%22%3A%20830217%7D",
-    {
-      headers: {
-        browser_name: "CHROME",
-        browser_version: 113,
-        shahid_os: "WINDOWS"
-      }
+    (drmResponse) => {
+      let data;
+      drmResponse.on("data", (chunk) => data += chunk.toString());
+      drmResponse.on("close", () => {
+        try {
+          const drm = JSON.parse(drmResponse);
+          if (!("signature" in drm)) throw null;
+          res.status(307).setHeader("location", drm.signature).end();
+        } catch (e) {
+          res.status(503).end();
+        }
+      });
     }
-  ).then((res) => res.json()).then((drm) => res.status(307).setHeader("location", drm.signature).end());
+  );
 
 }
